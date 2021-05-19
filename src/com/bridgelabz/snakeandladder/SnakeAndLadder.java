83c7d1e47ada3a1696eadb5404a684ece2d41524
anuj26298 <kumar26298anuj@gmail.com>
2021-05-19 17:14:59 +0530

    ADDED usecase 3

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -1,14 +1,15 @@
 package com.bridgelabz.snakeandladder;
 import java.util.Random;
 public class SnakeAndLadder {
+	public static final int FINAL_POSITION = 100;
+	
 	public static void main(String[] args) {
 		Random random = new Random();
 		//Variables
-		System.out.println("------------SNAKE AND LADDER----------------");
 		int positionPlayer1 = 0;
-		int diceNumber = random.nextInt(6) + 1;
-		
-		System.out.println(diceNumber);
+		int diceRollNum = random.nextInt(6) + 1;
+		int moveOption = random.nextInt(3);
+		System.out.println("------------SNAKE AND LADDER----------------");
 		
 		
 		
