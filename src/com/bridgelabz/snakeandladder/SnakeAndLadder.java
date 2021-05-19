da61bf193a0b024a15f41c2ff4bafc0d14d2a453
anuj26298 <kumar26298anuj@gmail.com>
2021-05-19 17:59:25 +0530

    UC3 using method/function

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -6,12 +6,29 @@ public class SnakeAndLadder {
 	public static void main(String[] args) {
 		Random random = new Random();
 		//Variables
-		int positionPlayer1 = 0;
+		int playerPosition = 0;
 		int diceRollNum = random.nextInt(6) + 1;
 		int moveOption = random.nextInt(3);
 		System.out.println("------------SNAKE AND LADDER----------------");
 		
 		
+		playerPosition = players(moveOption, diceRollNum);
 		
+		System.out.println("Player Position is:" + playerPosition);
+		
+		
+		
+	}
+	
+	public static int players(int moveOption, int diceRoll) {
+		int playerPosition = 0;
+		if(moveOption == 1) 
+			playerPosition +=diceRoll;
+		else if (moveOption == 2)
+			playerPosition -= diceRoll;
+		
+		if (playerPosition < 0)
+			playerPosition = 0;
+		return playerPosition;
 	}
 }
